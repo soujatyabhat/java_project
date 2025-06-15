e4373bd286d939ae320c7d0ac43fe49b22b1c04e
Soujatya Bhattacharya <65819358+soujatyabhat@users.noreply.github.com>
2025-06-15 10:00:11 +0530

    Update app.java
    New Code Added

--- app.java
+++ app.java
@@ -1,12 +1,5 @@
-import java.util.Scanner;
-
-public class Factorial {
+public class HelloWorld {
     public static void main(String[] args) {
-        int num = 5;
-        long fact = 1;
-        for (int i = 1; i <= num; i++) {
-            fact *= i;
-        }
-        System.out.println("Factorial of " + num + " is " + fact);
+        System.out.println("Hello, World!");
     }
 }
