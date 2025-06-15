44ba4e493fbf3467b6392e6bbb505c3461472aa7
Soujatya Bhattacharya <65819358+soujatyabhat@users.noreply.github.com>
2025-06-15 09:40:25 +0530

    Update app.java
    New Code Commit

--- app.java
+++ app.java
@@ -1,7 +1,13 @@
-import java.util.Scanner:
+import java.util.Scanner;
 
-public class Calculator {
-  public static void main(String[] args){
-      System.out.println("Hello Soujatya");
-  }
+public class Factorial {
+    public static void main(String[] args) {
+        int num = 5;
+        long fact = 1;
+        for (int i = 1; i <= num; i++) {
+            fact *= i;
+        }
+        System.out.println("Factorial of " + num + " is " + fact);
+        sc.close();
+    }
 }
